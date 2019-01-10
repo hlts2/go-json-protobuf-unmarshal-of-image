558440b104d0237412a580d61da70678e450e661
hlts2 <hiroto.funakoshi.hiroto@gmail.com>
2019-01-10 16:11:43 +0900

    Added processing to get file data and fix protobuf

--- main.go
+++ main.go
@@ -1,7 +1,51 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"path/filepath"
+
+	"github.com/pkg/errors"
+)
+
+// ImageRequest --
+type ImageRequest struct {
+	Images []Image
+}
+
+// Image --
+type Image struct {
+	UUID    string
+	Encoded string
+}
+
+func getFiles(dirPath string, filesLimit int) (map[string][]byte, error) {
+	infos, err := ioutil.ReadDir(dirPath)
+	if err != nil {
+		return nil, errors.Wrap(err, "faild to read directory")
+	}
+
+	files := make(map[string][]byte, len(infos))
+
+	for _, info := range infos {
+		path := filepath.Join(dirPath, info.Name())
+		d, err := ioutil.ReadFile(path)
+		if err != nil {
+			return nil, errors.Wrap(err, "faild to read file")
+		}
+		files[path] = d
+	}
+	return files, nil
+}
 
 func main() {
-	fmt.Println("vim-go")
+	counts := []int{4, 10, 20}
+	for _, count := range counts {
+		m, err := getFiles("./images", count)
+		if err != nil {
+			log.Fatal(err)
+		}
+		fmt.Println(m)
+	}
 }
