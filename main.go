aa5075b23ba577a6f21c714915c6ddecb01ca75f
hlts2 <hiroto.funakoshi.hiroto@gmail.com>
2019-01-10 16:46:06 +0900

    Added proto performance

--- main.go
+++ main.go
@@ -1,17 +1,22 @@
 package main
 
 import (
+	"encoding/base64"
+	"encoding/json"
 	"fmt"
 	"io/ioutil"
 	"log"
 	"path/filepath"
+	"time"
 
+	"github.com/gogo/protobuf/proto"
+	pb "github.com/hlts2/protobuf_unmarshal_v2_json_unmarshal/proto"
 	"github.com/pkg/errors"
 )
 
 // ImageRequest --
 type ImageRequest struct {
-	Images []Image
+	Images []*Image
 }
 
 // Image --
@@ -40,6 +45,48 @@ func getFiles(dirPath string, filesLimit int) (map[string][]byte, error) {
 	return files, nil
 }
 
+func jsonPerformance(data []byte) {
+	start := time.Now()
+	req := ImageRequest{}
+	json.Unmarshal(data, &req)
+	fmt.Println(time.Since(start))
+}
+
+func protoPerformance(data []byte) {
+	start := time.Now()
+	req := &pb.ImageRequest{}
+	proto.Unmarshal(data, req)
+	fmt.Println(time.Since(start))
+}
+
+func getJSONData(filesMap map[string][]byte) ([]byte, error) {
+	request := &ImageRequest{
+		Images: make([]*Image, 0, len(filesMap)),
+	}
+
+	for uuid, d := range filesMap {
+		request.Images = append(request.Images, &Image{
+			UUID:    uuid,
+			Encoded: base64.StdEncoding.EncodeToString(d),
+		})
+	}
+	return json.Marshal(request)
+}
+
+func getProtoData(filesMap map[string][]byte) ([]byte, error) {
+	request := &pb.ImageRequest{
+		Images: make([]*pb.ImageRequest_Image, 0, len(filesMap)),
+	}
+
+	for uuid, d := range filesMap {
+		request.Images = append(request.Images, &pb.ImageRequest_Image{
+			Uuid: uuid,
+			Data: d,
+		})
+	}
+	return proto.Marshal(request)
+}
+
 func main() {
 	counts := []int{4, 10, 20}
 	for _, count := range counts {
@@ -47,6 +94,20 @@ func main() {
 		if err != nil {
 			log.Fatal(err)
 		}
-		fmt.Println(m)
+
+		jd, err := getJSONData(m)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		jsonPerformance(jd)
+
+		pbd, err := getProtoData(m)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		protoPerformance(pbd)
+		break
 	}
 }
